d4906636cb11db3a3bb02d9c4eeda9aaf5d2958b
Ayaan <onestar7984@gmail.com>
2024-09-21 17:12:19 +0900

    feat: ColorStyles, TextStyles 구현.

---
+++ lib/common/color_styles.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+final class ColorStyles {
+  ColorStyles._();
+
+  //Primary Colors
+  static const Color black = Color(0x00000000);
+  static const Color white = Color(0xffffffff);
+  static const Color red = Color(0xffFE2E00);
+
+  //Grayscale
+  static const Color gray = Color(0xffFE2E00);
+  static const Color gray90 = Color(0xff313131);
+  static const Color gray80 = Color(0xff4F4F4F);
+  static const Color gray70 = Color(0xff626262);
+  static const Color gray60 = Color(0xff898989);
+  static const Color gray50 = Color(0xffAAAAAA);
+  static const Color gray40 = Color(0xffCFCFCF);
+  static const Color gray30 = Color(0xffE1E1E1);
+  static const Color gray20 = Color(0xffEEEEEE);
+  static const Color gray10 = Color(0xffF7F7F7);
+
+  //Opacity Black
+  static Color black70 = const Color(0x00000000).withOpacity(0.7);
+  static Color black50 = const Color(0x00000000).withOpacity(0.5);
+  static Color black30 = const Color(0x00000000).withOpacity(0.3);
+
+  //Opacity White
+  static Color white70 = const Color(0xffffffff).withOpacity(0.7);
+  static Color white50 = const Color(0xffffffff).withOpacity(0.5);
+  static Color white30 = const Color(0xffffffff).withOpacity(0.3);
+}
---
+++ lib/common/text_styles.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+final class TextStyles {
+  TextStyles._();
+
+  //HeadLine
+  static const headlineLargeBold = TextStyle(fontWeight: FontWeight.w700, fontSize: 24);
+
+  //Title
+  static const titleLargeBold = TextStyle(fontWeight: FontWeight.w700, fontSize: 16);
+  static const titleLargeSemiBold = TextStyle(fontWeight: FontWeight.w600, fontSize: 16);
+  static const titleMediumSemiBold = TextStyle(fontWeight: FontWeight.w600, fontSize: 15);
+  static const titleSmallBold = TextStyle(fontWeight: FontWeight.w700, fontSize: 14);
+
+  //Label
+  static const labelMediumRegular = TextStyle(fontWeight: FontWeight.w400, fontSize: 13);
+  static const labelMediumMedium = TextStyle(fontWeight: FontWeight.w500, fontSize: 13);
+  static const labelSmallRegular = TextStyle(fontWeight: FontWeight.w400, fontSize: 12);
+  static const labelSmallMedium = TextStyle(fontWeight: FontWeight.w500, fontSize: 12);
+  static const labelSmallSemiBold = TextStyle(fontWeight: FontWeight.w600, fontSize: 12);
+
+  //Text
+  static const textMediumRegular = TextStyle(fontWeight: FontWeight.w400, fontSize: 13);
+}
