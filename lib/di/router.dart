1c562dae3806ace2add5cfddebc71e1f15b9d00f
Ayaan <onestar7984@gmail.com>
2024-09-23 18:28:41 +0900

    feat: Router틀 구현

---
+++ lib/di/router.dart
@@ -0,0 +1,36 @@
+import 'package:brew_buds/main/main_view.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+const String initialPath = '/';
+
+final router = GoRouter(
+  initialLocation: initialPath,
+  routes: [
+    StatefulShellRoute.indexedStack(
+      builder: (context, state, bottomNavigationShell) => MainView(navigationShell: bottomNavigationShell),
+      branches: [
+        StatefulShellBranch(//홈 화면
+          routes: [
+            GoRoute(path: '/main1', builder: (context, state) => Container()),
+          ],
+        ),
+        StatefulShellBranch(//검색 화면
+          routes: [
+            GoRoute(path: '/main2', builder: (context, state) => Container()),
+          ],
+        ),
+        StatefulShellBranch(//기록 화면
+          routes: [
+            GoRoute(path: '/main3', builder: (context, state) => Container()),
+          ],
+        ),
+        StatefulShellBranch(//프로필 화면
+          routes: [
+            GoRoute(path: '/main4', builder: (context, state) => Container()),
+          ],
+        ),
+      ],
+    )
+  ],
+);
