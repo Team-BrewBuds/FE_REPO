5182e4f5aa582c980e9443c6c33c85b8e295c26e
Ayaan <onestar7984@gmail.com>
2024-10-12 15:23:05 +0900

    refactor: Text Style 추가 및 수정
    - titleLargeBold -> title02Bold 네이밍 변경
    - captionXSmallMedium 추가

--- lib/common/text_styles.dart
+++ lib/common/text_styles.dart
@@ -7,7 +7,7 @@ final class TextStyles {
   static const headlineLargeBold = TextStyle(fontWeight: FontWeight.w700, fontSize: 24);
 
   //Title
-  static const titleLargeBold = TextStyle(fontWeight: FontWeight.w700, fontSize: 16);
+  static const title02Bold = TextStyle(fontWeight: FontWeight.w700, fontSize: 16);
   static const titleLargeSemiBold = TextStyle(fontWeight: FontWeight.w600, fontSize: 16);
   static const titleMediumSemiBold = TextStyle(fontWeight: FontWeight.w600, fontSize: 15);
   static const titleSmallBold = TextStyle(fontWeight: FontWeight.w700, fontSize: 14);
@@ -23,4 +23,5 @@ final class TextStyles {
   static const textMediumRegular = TextStyle(fontWeight: FontWeight.w400, fontSize: 13);
 
   static const captionMediumMedium = TextStyle(fontWeight: FontWeight.w500, fontSize: 12);
+  static const captionXSmallMedium = TextStyle(fontWeight: FontWeight.w500, fontSize: 8);
 }
