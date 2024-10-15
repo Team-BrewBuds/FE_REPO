7fe3829c5d82c1b33bab2bbe2f79b3a52c197c3e
Ayaan <onestar7984@gmail.com>
2024-10-16 03:12:31 +0900

    refactor: 추천 유저를 활용해 특정 index에서 추천 유저 표시하도록 구현

--- lib/home/core/home_view_mixin.dart
+++ lib/home/core/home_view_mixin.dart
@@ -1,5 +1,7 @@
 import 'package:brew_buds/common/color_styles.dart';
+import 'package:brew_buds/common/text_styles.dart';
 import 'package:brew_buds/home/core/home_view_presenter.dart';
+import 'package:brew_buds/home/widgets/follow_button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -36,13 +38,11 @@ mixin HomeViewMixin<T extends StatefulWidget, Presenter extends HomeViewPresente
   Widget _buildRefreshWidget(Presenter presenter) {
     return CupertinoSliverRefreshControl(
       onRefresh: presenter.onRefresh,
-      builder: (
-        BuildContext context,
-        RefreshIndicatorMode refreshState,
-        double pulledExtent,
-        double refreshTriggerPullDistance,
-        double refreshIndicatorExtent,
-      ) {
+      builder: (BuildContext context,
+          RefreshIndicatorMode refreshState,
+          double pulledExtent,
+          double refreshTriggerPullDistance,
+          double refreshIndicatorExtent,) {
         switch (refreshState) {
           case RefreshIndicatorMode.armed || RefreshIndicatorMode.refresh:
             return Container(
@@ -61,25 +61,100 @@ mixin HomeViewMixin<T extends StatefulWidget, Presenter extends HomeViewPresente
   SliverList _buildListView(Presenter presenter) {
     return SliverList.separated(
       itemCount: presenter.feeds.length,
-      itemBuilder: (context, index) => buildListItem(presenter, index),
-      separatorBuilder: (context, index) => _buildSeparator(presenter, index),
+      itemBuilder: (context, index) {
+        if (index % 12 == 11) {
+          return Column(
+            children: [
+              buildListItem(presenter, index),
+              Container(height: 12, color: ColorStyles.gray20),
+              _buildRemandedBuddies(presenter),
+            ],
+          );
+        } else {
+          return buildListItem(presenter, index);
+        }
+      },
+      separatorBuilder: (context, index) => Container(height: 12, color: ColorStyles.gray20),
     );
   }
 
-  Widget _buildSeparator(Presenter presenter, int index) {
-    if (index % 12 == 11) {
-      return _buildRemandedBuddies(presenter);
-    } else {
-      return Container(height: 12, color: ColorStyles.gray50);
-    }
+  Widget _buildRemandedBuddies(Presenter presenter) {
+    return Container(
+      height: 300,
+      color: ColorStyles.white,
+      padding: const EdgeInsets.only(left: 16, right: 16, top: 20, bottom: 24),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Text('카페 투어를 즐기는 버디', style: TextStyles.title01SemiBold),
+          Text('오늘은 새로운 버디와 카페 투어 어때요?', style: TextStyles.bodyRegular.copyWith(color: ColorStyles.gray70),),
+          const SizedBox(height: 16),
+          Expanded(
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: presenter.remandedUsers.length,
+              itemBuilder: (context, index) =>
+                  _buildRemandedBuddyProfile(
+                    imageUri: presenter.remandedUsers[index].thumbnailUri,
+                    nickName: presenter.remandedUsers[index].nickName,
+                    followCount: '${presenter.remandedUsers[index].followCount}',
+                    isFollowed: presenter.remandedUsers[index].isFollowed,
+                  ),
+              separatorBuilder: (context, index) => const SizedBox(width: 8),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
-  Widget _buildRemandedBuddies(Presenter presenter) {
-    if (presenter.remandedUsers.isNotEmpty) {
-      //추천 버디 UI
-      return Container(height: 290, color: Colors.red,);
-    } else {
-      return Container(height: 12, color: ColorStyles.gray50);
-    }
+  Widget _buildRemandedBuddyProfile({
+    required String imageUri,
+    required String nickName,
+    required String followCount,
+    required bool isFollowed,
+  }) {
+    return Container(
+      width: 134,
+      padding: const EdgeInsets.symmetric(vertical: 12),
+      decoration: BoxDecoration(
+        color: ColorStyles.white,
+        border: Border.all(color: ColorStyles.gray20),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        children: [
+          Container(
+            height: 80,
+            width: 80,
+            clipBehavior: Clip.antiAlias,
+            decoration: const BoxDecoration(
+              color: Color(0xffD9D9D9),
+              shape: BoxShape.circle,
+            ),
+            child: Image.network(imageUri, fit: BoxFit.cover),
+          ),
+          const SizedBox(height: 12),
+          Text(
+            nickName,
+            style: TextStyles.labelSmallSemiBold,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+          ),
+          const SizedBox(height: 2),
+          Text(
+            followCount,
+            style: TextStyles.labelSmallSemiBold,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+          ),
+          const SizedBox(height: 12),
+          FollowButton(onTap: () {}, isFollowed: isFollowed),
+        ],
+      ),
+    );
   }
 }
+
+
