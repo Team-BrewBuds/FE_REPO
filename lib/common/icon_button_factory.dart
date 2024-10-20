582bc15c1eb01d31b73999a0450789505cbc5583
Ayaan <onestar7984@gmail.com>
2024-10-20 21:19:26 +0900

    refactor: Icon Button Factory Method 수정
    - 아이콘을 직접 받아서 사용
    - 크기가 고정적이지 않도록 구현

--- lib/common/icon_button_factory.dart
+++ lib/common/icon_button_factory.dart
@@ -1,18 +1,4 @@
-import 'package:brew_buds/common/color_styles.dart';
-import 'package:brew_buds/common/text_styles.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-
-class IconButtonStyle {
-  final Color textColor;
-  final ButtonIconAlign iconAlign;
-
-  const IconButtonStyle({
-    this.textColor = ColorStyles.white,
-    required this.iconAlign,
-  });
-}
 
 enum ButtonIconAlign { left, right }
 
@@ -20,24 +6,28 @@ final class IconButtonFactory {
   IconButtonFactory._();
 
   static Widget buildHorizontalButtonWithIcon({
-    required IconData iconData,
+    required Icon icon,
     required String text,
+    required TextStyle textStyle,
     required Function() onTapped,
-    required IconButtonStyle style,
+    required ButtonIconAlign iconAlign,
   }) {
     return InkWell(
       onTap: onTapped,
-      child: Row(
-        children: switch (style.iconAlign) {
-          ButtonIconAlign.left => [
-              Icon(iconData, color: style.textColor, size: 18),
-              Text(text, style: TextStyles.labelMediumMedium.copyWith(color: style.textColor)),
-            ],
-          ButtonIconAlign.right => [
-              Text(text, style: TextStyles.labelMediumMedium.copyWith(color: style.textColor)),
-              Icon(iconData, color: style.textColor, size: 18),
-            ],
-        },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 2),
+        child: Row(
+          children: switch (iconAlign) {
+            ButtonIconAlign.left => [
+                icon,
+                Text(text, style: textStyle),
+              ],
+            ButtonIconAlign.right => [
+                Text(text, style: textStyle),
+                icon,
+              ],
+          },
+        ),
       ),
     );
   }
@@ -45,22 +35,26 @@ final class IconButtonFactory {
   static Widget buildHorizontalButtonWithIconWidget({
     required Widget iconWidget,
     required String text,
+    required TextStyle textStyle,
     required Function() onTapped,
-    required IconButtonStyle style,
+    required ButtonIconAlign iconAlign,
   }) {
     return InkWell(
       onTap: onTapped,
-      child: Row(
-        children: switch (style.iconAlign) {
-          ButtonIconAlign.left => [
-              SizedBox(height: 24, width: 24, child: iconWidget),
-              Text(text, style: TextStyles.labelMediumMedium.copyWith(color: style.textColor)),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 2),
+        child: Row(
+          children: switch (iconAlign) {
+            ButtonIconAlign.left => [
+              iconWidget,
+              Text(text, style: textStyle),
             ],
-          ButtonIconAlign.right => [
-              Text(text, style: TextStyles.labelMediumMedium.copyWith(color: style.textColor)),
-              SizedBox(height: 24, width: 24, child: iconWidget),
+            ButtonIconAlign.right => [
+              Text(text, style: textStyle),
+              iconWidget,
             ],
-        },
+          },
+        ),
       ),
     );
   }
@@ -68,18 +62,18 @@ final class IconButtonFactory {
   static Widget buildVerticalButtonWithIconWidget({
     required Widget iconWidget,
     required String text,
+    required TextStyle textStyle,
     required Function() onTapped,
-    required Color color,
   }) {
     return InkWell(
       onTap: onTapped,
-      child: SizedBox(
-        height: 49,
-        width: 36,
-        child: Stack(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 14, left: 6, right: 6),
+        child: Column(
           children: [
-            Positioned(top: 14, left: 16, right: 16, child: SizedBox(height: 18, width: 18, child: iconWidget)),
-            Positioned(bottom: 0, child: Text(text, style: TextStyles.captionMediumMedium.copyWith(color: color))),
+            Center(child: iconWidget),
+            SizedBox(height: 2),
+            Text(text, style: textStyle),
           ],
         ),
       ),
