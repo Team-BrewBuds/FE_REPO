2eaf757f04e0a38f5843e6bcbe320f7d2374c7ef
Ayaan <onestar7984@gmail.com>
2024-10-26 18:32:36 +0900

    feat: .env 관리를 위한 Library 추가

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import 'package:brew_buds/di/router.dart';
 import 'package:brew_buds/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
