8f2c174272f7d043d97cd0159a6e9d77901f4adb
Ayaan <onestar7984@gmail.com>
2024-11-14 23:25:47 +0900

    refactor: 디렉토리 구조 변경 및 FilterPresenter 구현

--- lib/profile/presenter/profile_presenter.dart
+++ lib/profile/presenter/profile_presenter.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+import 'package:syncfusion_flutter_sliders/sliders.dart';
 
 enum SortCriteria {
   upToDate,
@@ -18,6 +19,10 @@ enum SortCriteria {
       };
 }
 
+enum BeanType { singleOrigin, blend }
+
+enum BeanOrigin { none }
+
 class ProfilePresenter extends ChangeNotifier {
   final List<SortCriteria> _sortCriteriaList = [
     SortCriteria.upToDate,
@@ -25,8 +30,15 @@ class ProfilePresenter extends ChangeNotifier {
     SortCriteria.tastingRecords,
   ];
   int _currentSortCriteriaIndex = 0;
+  SfRangeValues _ratingValues = SfRangeValues(0.5, 5.0);
+  SfRangeValues _roastingPointValues = SfRangeValues(1, 5);
 
   List<SortCriteria> get sortCriteriaList => _sortCriteriaList;
+
+  String get ratingString => '${_ratingValues.start}점 ~ ${_ratingValues.end}점';
+
+  String get roastingPointString =>
+      '${_toRoastingPointString(_roastingPointValues.start)} ~ ${_toRoastingPointString(_roastingPointValues.end)}';
 
   String get currentSortCriteria => _sortCriteriaList[_currentSortCriteriaIndex].buttonString;
 
@@ -35,5 +47,32 @@ class ProfilePresenter extends ChangeNotifier {
   onChangeSortCriteriaIndex(int index) {
     _currentSortCriteriaIndex = index;
     notifyListeners();
+  }
+
+  onChangeRatingValues(SfRangeValues values) {
+    _ratingValues = values;
+    notifyListeners();
+  }
+
+  onChangeRoastingPointValues(SfRangeValues values) {
+    _roastingPointValues = values;
+    notifyListeners();
+  }
+
+  String _toRoastingPointString(double value) {
+    switch (value) {
+      case 1:
+        return '라이트';
+      case 2:
+        return '라이트 미디엄';
+      case 3:
+        return '미디엄';
+      case 4:
+        return '미디엄 다크';
+      case 5:
+        return '다크';
+      default:
+        return '';
+    }
   }
 }
