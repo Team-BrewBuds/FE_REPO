8905f5f6aed054ebfbfd3a5bc648142ee0477191
kkkjj5199 <kkkjj5199@naver.com>
2024-11-25 17:13:09 +0900

    profile model 필드 수정

--- lib/model/profile.dart
+++ lib/model/profile.dart
@@ -14,10 +14,10 @@ class Profile with _$Profile {
     @JsonKey(name: 'profile_image', defaultValue: '') String? profileImageUri,
     @JsonKey() String ? introduction,
     @JsonKey() String ? profile_link,
-    @JsonKey()  Map<String,Map<String,dynamic>>? coffLife,
-    @JsonKey(defaultValue: 0)  int ?followingCnt,
-    @JsonKey(defaultValue: 0) int ?followerCnt,
-    @JsonKey(defaultValue: 0)  int ?postCnt,
+    @JsonKey(name:'coffee_life') required Map<String,dynamic>  coffLife,
+    @JsonKey(name:'following_cnt')  int ?followingCnt,
+    @JsonKey(name:'follower_cnt') int ?followerCnt,
+    @JsonKey(name: 'post_cnt')  int ?postCnt,
     @JsonKey(defaultValue: false) required bool isUserFollowing,
     @JsonKey(defaultValue: false) required bool isUserBlocking
 
