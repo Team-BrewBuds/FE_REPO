2c17542b2e7ebe11006ac91c8ca90b9c721518ad
Ayaan <onestar7984@gmail.com>
2024-11-29 01:24:28 +0900

    refactor: 회원가입 두번째 페이지 수정

--- lib/features/signup/views/signup_page_enjoy.dart
+++ lib/features/signup/views/signup_page_enjoy.dart
@@ -1,180 +1,104 @@
 import 'package:brew_buds/common/color_styles.dart';
-import 'package:brew_buds/features/signup/models/signup_lists.dart';
+import 'package:brew_buds/common/text_styles.dart';
+import 'package:brew_buds/features/signup/models/coffee_life.dart';
 import 'package:brew_buds/features/signup/provider/sign_up_presenter.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:brew_buds/features/signup/views/signup_mixin.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
-class SignUpEnjoy extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: CoffeeLifestylePage(),
-    );
-  }
-}
-
 class CoffeeLifestylePage extends StatefulWidget {
+  const CoffeeLifestylePage({super.key});
+
   @override
   State<CoffeeLifestylePage> createState() => _CoffeeLifestylePageState();
 }
 
-class _CoffeeLifestylePageState extends State<CoffeeLifestylePage> {
-  final SignUpLists lists = SignUpLists();
-
-  List<bool> selectedItems = List.generate(6, (_) => false);
-  List<String> selectedChoices = [];
+class _CoffeeLifestylePageState extends State<CoffeeLifestylePage> with SignupMixin<CoffeeLifestylePage> {
+  @override
+  int get currentPageIndex => 1;
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        scrolledUnderElevation: 0,
-        backgroundColor: Colors.white,
-        leading: IconButton(
-          icon: Icon(CupertinoIcons.back),
-          onPressed: () => Navigator.of(context, rootNavigator: true).pop(),
-        ),
-        title: Text('회원가입'),
-      ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          physics: ClampingScrollPhysics(),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(right: 2),
-                      child: Container(
-                        width: 84.25,
-                        height: 2,
-                        decoration: BoxDecoration(color: Color(0xFFFE2D00)),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(right: 2),
-                      child: Container(
-                        width: 84.25,
-                        height: 2,
-                        decoration: BoxDecoration(color: Color(0xFFFE2D00)),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(right: 2),
-                      child: Container(
-                        width: 84.25,
-                        height: 2,
-                        decoration: BoxDecoration(color: Color(0xFFCFCFCF)),
-                      ),
-                    ),
-                    Container(
-                      width: 84.25,
-                      height: 2,
-                      decoration: BoxDecoration(color: Color(0xFFCFCFCF)),
-                    )
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text(
-                  '커피 생활을 어떻게 즐기세요?',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: Text('최대 6개까지 선택할 수 있어요.'),
-              ),
-              GridView.builder(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                padding: EdgeInsets.all(16),
-                itemCount: lists.enjoyItems.length,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 0.8,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                ),
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        selectedItems[index] = !selectedItems[index];
-                        // 선택된 아이템을 업데이트
-                        if (selectedItems[index]) {
-                          selectedChoices.add(lists.enjoyItems[index]['choice']!);
-                        } else {
-                          selectedChoices.remove(lists.enjoyItems[index]['choice']!);
-                        }
+  bool get isSatisfyRequirements => context.read<SignUpPresenter>().selectedCoffeeLife.isNotEmpty;
 
-                        print(selectedChoices);
-                      });
-                    },
-                    child: Card(
-                      elevation: 0,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                        side: BorderSide(
-                          color: selectedItems[index] ? Colors.red : ColorStyles.gray30,
-                          width: 2,
+  @override
+  bool get isSkippablePage => true;
+
+  @override
+  void Function() get onNext => () {
+        context.push('/signup/cert');
+      };
+
+  @override
+  void Function() get onSkip => () {};
+
+  @override
+  Widget buildBody(BuildContext context, SignUpPresenter presenter) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('커피 생활을 어떻게 즐기세요?', style: TextStyles.title04SemiBold),
+        const SizedBox(height: 4),
+        Text('최대 6개까지 선택할 수 있어요.', style: TextStyles.bodyRegular.copyWith(color: ColorStyles.gray50)),
+        const SizedBox(height: 14),
+        Expanded(
+          child: GridView.builder(
+            shrinkWrap: true,
+            itemCount: CoffeeLife.values.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 8,
+              mainAxisSpacing: 8,
+              childAspectRatio: 0.9,
+            ),
+            itemBuilder: (context, index) {
+              return InkWell(
+                onTap: () {
+                  presenter.onSelectCoffeeLife(CoffeeLife.values[index]);
+                },
+                child: Container(
+                  padding: const EdgeInsets.only(top: 4, bottom: 8, left: 16, right: 16),
+                  decoration: BoxDecoration(
+                    color: presenter.selectedCoffeeLife.contains(CoffeeLife.values[index])
+                        ? ColorStyles.background
+                        : ColorStyles.white,
+                    border: Border.all(
+                        color: presenter.selectedCoffeeLife.contains(CoffeeLife.values[index])
+                            ? ColorStyles.red
+                            : ColorStyles.gray50),
+                    borderRadius: const BorderRadius.all(Radius.circular(8)),
+                  ),
+                  child: Column(
+                    children: [
+                      const Spacer(),
+                      Image.asset(CoffeeLife.values[index].imagePath, width: 120, height: 86, fit: BoxFit.cover),
+                      const SizedBox(height: 4),
+                      Text(
+                        CoffeeLife.values[index].title,
+                        style: const TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.w700,
+                          height: 15.6 / 13,
+                          letterSpacing: -0.01,
                         ),
                       ),
-                      color: selectedItems[index] ? Color(0xFFFFF7F5) : Colors.white,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset("assets/images/${lists.enjoyItems[index]['png']}.png"),
-                          SizedBox(height: 10),
-                          Text(
-                            lists.enjoyItems[index]['title']!,
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              lists.enjoyItems[index]['description']!,
-                              textAlign: TextAlign.center,
-                              style: TextStyle(fontSize: 12),
-                            ),
-                          ),
-                        ],
+                      const SizedBox(height: 2),
+                      Text(
+                        CoffeeLife.values[index].description,
+                        style: TextStyles.captionSmallMedium.copyWith(
+                          color: ColorStyles.gray60,
+                        ),
+                        textAlign: TextAlign.center,
                       ),
-                    ),
-                  );
-                },
-              ),
-            ],
+                      const Spacer(),
+                    ],
+                  ),
+                ),
+              );
+            },
           ),
         ),
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.only(bottom: 46.0, left: 16, right: 16),
-        child: SizedBox(
-          width: double.infinity,
-          child: ElevatedButton(
-            child: Text('다음'),
-            onPressed: selectedChoices.isNotEmpty
-                ? () {
-                    context.read<SignUpPresenter>().getCoffeeLife(selectedChoices);
-                    context.push("/signup/cert");
-                  }
-                : null, // 선택된 아이템이 없으면 비활성화
-            style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(vertical: 15),
-                backgroundColor: selectedChoices.isNotEmpty ? Colors.black : ColorStyles.gray30,
-                foregroundColor: ColorStyles.white,
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
-          ),
-        ),
-      ),
+      ],
     );
   }
 }
