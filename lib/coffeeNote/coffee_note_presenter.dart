dcc9a5098578ffdb8034b9accdaa134b58e85425
kkkjj5199 <kkkjj5199@naver.com>
2025-01-06 23:36:15 +0900

    커피노트 글쓰기 : 간단한 리팩토링

--- lib/coffeeNote/coffee_note_presenter.dart
+++ lib/coffeeNote/coffee_note_presenter.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:brew_buds/coffeeNote/controller/custom_controller.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:image_picker/image_picker.dart';
 
 final class CoffeeNotePresenter extends ChangeNotifier {
   TextEditingController titleController = TextEditingController();
@@ -12,41 +15,34 @@ final class CoffeeNotePresenter extends ChangeNotifier {
   List<String> _tag = [];
   List<String> get tag => _tag;
 
+
   // 제목 설정
   void setTitle(String title) {
     _title = title;
     notifyListeners();
   }
 
-  // 태그 추가
-  void addTag(String tag) {
-    if (!_tag.contains(tag)) {
-      _tag.add(tag);
-      notifyListeners(); // 태그 리스트 업데이트 후 UI 갱신
-    }
-  }
-
-  // 태그 제거
-  void removeTag(String tag) {
-    _tag.remove(tag);
-    notifyListeners(); // 태그 리스트 업데이트 후 UI 갱신
-  }
-
-  // 태그 수를 체크하고, 제한을 넘지 않도록 하기 위한 메서드
-  void setTagCount(int tagCount) {
-    // 태그 개수 제한을 설정하는 메서드 (예: 최대 30개)
-    if (tagCount > 3) {
-      // 태그가 30개를 초과하면 30개까지만 유지
-      _tag = _tag.sublist(0, 3);
-    }
-    notifyListeners();
-  }
 
   List<String> extractTags(String text) {
     final RegExp regex = RegExp(r'#\w+');
     return regex.allMatches(text).map((match) => match.group(0)!).toList();
   }
 
+  File? _image; // 선택된 이미지를 저장할 변수
+
+  // 이미지 선택 함수
+  Future<void> pickImage() async {
+    final ImagePicker picker = ImagePicker();
+
+    // 갤러리에서 이미지 선택
+    final XFile? pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path); // 선택된 이미지 경로를 File로 저장
+      });
+    }
+  }
 
 
   @override
@@ -57,4 +53,6 @@ final class CoffeeNotePresenter extends ChangeNotifier {
     customTagController.dispose();
     super.dispose();
   }
+
+  void setState(Null Function() param0) {}
 }
