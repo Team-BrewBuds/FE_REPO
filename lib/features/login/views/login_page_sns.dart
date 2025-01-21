8108d69942b980e73c81f7f7d52d18c5846cccbf
Ayaan <onestar7984@gmail.com>
2025-01-21 17:42:26 +0900

    refactor: Login화면 QA

--- lib/features/login/views/login_page_sns.dart
+++ lib/features/login/views/login_page_sns.dart
@@ -25,147 +25,187 @@ class _SNSLoginState extends State<SNSLogin> {
   @override
   Widget build(BuildContext context) {
     const height = 54.0;
-    return Scaffold(
-      appBar: AppBar(),
-      body: Consumer<LoginPresenter>(builder: (context, presenter, _) {
-        return AbsorbPointer(
-          absorbing: presenter.isLoading,
-          child: Stack(
-            children: [
-              Center(
-                child: presenter.isLoading ? const CircularProgressIndicator(color: ColorStyles.gray60) : Container(),
-              ),
-              Positioned.fill(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        '간편로그인으로\n빠르게 가입하세요.',
-                        style: TextStyles.title05Bold,
-                      ),
-                      const SizedBox(height: 48),
-                      Column(
-                        children: [
-                          SizedBox(
-                            height: height, // 버튼 높이 통일
-                            child: ElevatedButton(
-                              onPressed: () {
-                                _checkModal(presenter, SocialLogin.kakao); // Kakao 로그인 로직
+    return Consumer<LoginPresenter>(
+      builder: (context, presenter, _) {
+        return Scaffold(
+          appBar: _buildAppBar(presenter),
+          body: AbsorbPointer(
+            absorbing: presenter.isLoading,
+            child: Stack(
+              children: [
+                Center(
+                  child: presenter.isLoading ? const CircularProgressIndicator(color: ColorStyles.gray60) : Container(),
+                ),
+                Positioned.fill(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          '간편로그인으로\n빠르게 가입하세요.',
+                          style: TextStyles.title05Bold,
+                        ),
+                        const SizedBox(height: 48),
+                        Column(
+                          children: [
+                            InkWell(
+                              onTap: () {
+                                _checkModal(presenter, SocialLogin.kakao);
                               },
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: const Color(0xFFFFE812), // Kakao 배경 색상
-                                shape: RoundedRectangleBorder(
+                              child: Container(
+                                height: height, // 버튼 높이 통일
+                                padding: const EdgeInsets.symmetric(horizontal: 14),
+                                decoration: BoxDecoration(
+                                  color: const Color(0xFFFFE812),
                                   borderRadius: BorderRadius.circular(6),
                                 ),
-                              ),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  SvgPicture.asset(
-                                    'assets/icons/kakao.svg',
-                                    width: 16,
-                                    height: 16,
-                                  ),
-                                  const SizedBox(width: 8),
-                                  const Text(
-                                    '카카오로 로그인',
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.w600,
-                                      fontSize: 15,
-                                      height: 22.5 / 15,
-                                      color: ColorStyles.white,
+                                child: Center(
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(horizontal: 24),
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      children: [
+                                        SvgPicture.asset(
+                                          'assets/icons/kakao.svg',
+                                          width: 18,
+                                          height: 18,
+                                        ),
+                                        const SizedBox(width: 8),
+                                        const Text(
+                                          '카카오로 로그인',
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.w600,
+                                            fontSize: 15,
+                                            height: 22.5 / 15,
+                                            color: ColorStyles.black,
+                                          ),
+                                        ),
+                                      ],
                                     ),
                                   ),
-                                  const SizedBox(width: 8),
-                                ],
+                                ),
                               ),
                             ),
-                          ),
-                          const SizedBox(height: 7),
-                          SizedBox(
-                            height: height, // 버튼 높이 통일
-                            child: ElevatedButton(
-                              onPressed: () {
+                            const SizedBox(height: 7),
+                            InkWell(
+                              onTap: () {
                                 _checkModal(presenter, SocialLogin.naver);
                               },
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: const Color(0xFF03C75A), // 배경 색상
-                                shape: RoundedRectangleBorder(
+                              child: Container(
+                                height: height, // 버튼 높이 통일
+                                padding: const EdgeInsets.symmetric(horizontal: 14),
+                                decoration: BoxDecoration(
+                                  color: const Color(0xFF03C75A),
                                   borderRadius: BorderRadius.circular(6),
                                 ),
-                              ),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  SvgPicture.asset(
-                                    'assets/icons/naver.svg',
-                                    width: 16,
-                                    height: 16,
-                                  ),
-                                  const SizedBox(width: 8),
-                                  const Text(
-                                    '네이버로 로그인',
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.w600,
-                                      fontSize: 15,
-                                      height: 22.5 / 15,
-                                      color: ColorStyles.white,
+                                child: Center(
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(horizontal: 35),
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      children: [
+                                        SvgPicture.asset(
+                                          'assets/icons/naver.svg',
+                                          width: 16,
+                                          height: 16,
+                                        ),
+                                        const SizedBox(width: 15),
+                                        const Text(
+                                          '네이버로 로그인',
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.w600,
+                                            fontSize: 15,
+                                            height: 22.5 / 15,
+                                            color: ColorStyles.white,
+                                          ),
+                                        ),
+                                      ],
                                     ),
                                   ),
-                                  const SizedBox(width: 8),
-                                ],
+                                ),
                               ),
                             ),
-                          ),
-                          const SizedBox(height: 7),
-                          SizedBox(
-                            height: height, // 버튼 높이 통일
-                            child: ElevatedButton(
-                              onPressed: () {
-                                _checkModal(presenter, SocialLogin.apple);
+                            const SizedBox(height: 7),
+                            InkWell(
+                              onTap: () {
+                                _checkModal(presenter, SocialLogin.kakao);
                               },
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: const Color(0xFF000000), // 배경 색상
-                                shape: RoundedRectangleBorder(
+                              child: Container(
+                                height: height, // 버튼 높이 통일
+                                padding: const EdgeInsets.symmetric(horizontal: 14),
+                                decoration: BoxDecoration(
+                                  color: const Color(0xFF000000),
                                   borderRadius: BorderRadius.circular(6),
                                 ),
-                              ),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  SvgPicture.asset(
-                                    'assets/icons/apple_logo.svg',
-                                    width: 16,
-                                    height: 16,
-                                    colorFilter: const ColorFilter.mode(ColorStyles.white, BlendMode.srcIn),
-                                  ),
-                                  const SizedBox(width: 8),
-                                  const Text(
-                                    'Apple로 로그인',
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.w600,
-                                      fontSize: 15,
-                                      height: 22.5 / 15,
-                                      color: ColorStyles.white,
+                                child: Center(
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      children: [
+                                        SvgPicture.asset(
+                                          'assets/icons/apple_logo.svg',
+                                          width: 13,
+                                          height: 13,
+                                          colorFilter: const ColorFilter.mode(ColorStyles.white, BlendMode.srcIn),
+                                        ),
+                                        const SizedBox(width: 5),
+                                        const Text(
+                                          'Apple로 로그인',
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.w600,
+                                            fontSize: 15,
+                                            height: 22.5 / 15,
+                                            color: ColorStyles.white,
+                                          ),
+                                        ),
+                                      ],
                                     ),
                                   ),
-                                  const SizedBox(width: 8),
-                                ],
+                                ),
                               ),
                             ),
-                          ),
-                        ],
-                      ),
-                    ],
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         );
-      }),
+      },
+    );
+  }
+
+  AppBar _buildAppBar(LoginPresenter presenter) {
+    return AppBar(
+      leading: const SizedBox.shrink(),
+      leadingWidth: 0,
+      titleSpacing: 0,
+      title: Padding(
+        padding: const EdgeInsets.only(top: 35, left: 16, right: 16, bottom: 12),
+        child: Row(
+          children: [
+            InkWell(
+              onTap: () {
+                context.pop();
+              },
+              child: SvgPicture.asset(
+                'assets/icons/back.svg',
+                width: 24,
+                height: 24,
+              ),
+            )
+          ],
+        ),
+      ),
+      toolbarHeight: 67,
     );
   }
 
@@ -175,19 +215,23 @@ class _SNSLoginState extends State<SNSLogin> {
       shape: const RoundedRectangleBorder(
         borderRadius: BorderRadius.vertical(top: Radius.circular(8.0)),
       ),
-      constraints: const BoxConstraints(maxHeight: 421, minHeight: 421),
+      constraints: const BoxConstraints(maxHeight: 421),
       backgroundColor: ColorStyles.white,
       elevation: 0,
       builder: (BuildContext context) {
         return const TermsOfUseBottomSheet();
       },
-    ).then((result) {
-      if (result != null && result) {
-        presenter.socialLogin(socialLogin).then((result) => switch (result) {
-              LoginSuccess() => result.hasAccount ? context.go('/home/all') : context.push('/signup'),
-              LoginError() => throw UnimplementedError(), // 화면 에러처리
-            });
-      }
-    });
+    ).then(
+      (result) {
+        if (result != null && result) {
+          presenter.socialLogin(socialLogin).then(
+                (result) => switch (result) {
+                  LoginSuccess() => result.hasAccount ? context.go('/home/all') : context.push('/signup'),
+                  LoginError() => throw UnimplementedError(), // 화면 에러처리
+                },
+              );
+        }
+      },
+    );
   }
 }
