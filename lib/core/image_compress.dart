06a71eeed221b12ad61643c8119dc17454653c90
Ayaan <onestar7984@gmail.com>
2025-08-05 16:46:41 +0900

    refactor: Image 압축 방식 변경

--- lib/core/image_compress.dart
+++ lib/core/image_compress.dart
@@ -3,21 +3,11 @@ import 'dart:typed_data';
 import 'package:flutter_image_compress/flutter_image_compress.dart';
 
 Future<Uint8List> compressList(Uint8List list) async {
-  int minHeight = 1024;
-  int minWidth = 1024;
-  int maxLength = 1024 * 1024;
-  Uint8List result = list;
-
-  while (result.length > maxLength) {
-    result = await FlutterImageCompress.compressWithList(
-      list,
-      minHeight: minHeight,
-      minWidth: minWidth,
-      quality: 90,
-      format: CompressFormat.png,
-    );
-    minHeight = (minHeight * 0.9).round();
-    minWidth = (minWidth * 0.9).round();
-  }
-  return result;
+  return FlutterImageCompress.compressWithList(
+    list,
+    minHeight: 512,
+    minWidth: 512,
+    quality: 95,
+    format: CompressFormat.jpeg,
+  );
 }
